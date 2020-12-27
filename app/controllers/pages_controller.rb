3b501b68a06bec4d14c64bc40998a96f780f611b
Xavier Vieujean <xavier.vieujean@hotmail.ch>
2020-12-27 14:23:20 +0100

    static pages and their links in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :tandem, :training, :flightzone, :contact ]
 
   def home
+  end
+
+  def tandem
+  end
+
+  def training
+  end
+
+  def flightzone
+  end
+
+  def contact
   end
 end
